14fe75448542b0712c5faea4ab70eeebad1849d4
prabhsingh20 <singh20prabh@gmail.com>
2023-10-10 14:18:23 +0530

    auth

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("user@gmail.com");
+  const [password, setPassword] = useState("user1234");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
